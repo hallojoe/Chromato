fd9075547ebaac3878b7b9742cbe28fa8dd45516
Casper Korsgaard <cko@charlietango.dk>
2020-06-09 20:32:27 +0200

    Add build

---
+++ chromato-app/build/precache-manifest.28c59aa03457069b9299d368d85548ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f687c6890c10308ff2df3c7d9fc6dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "359d349ffac93e0c064c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30de2532c161de817d7c",
+    "url": "/static/js/2.305dc9c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.305dc9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359d349ffac93e0c064c",
+    "url": "/static/js/main.d909b390.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
