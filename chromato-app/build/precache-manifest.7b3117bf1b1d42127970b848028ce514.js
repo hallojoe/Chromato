be6347996ef14b5019c6c910d975c137d95c62c8
Casper Korsgaard <cko@charlietango.dk>
2020-06-09 21:14:06 +0200

    Update build

---
+++ chromato-app/build/precache-manifest.7b3117bf1b1d42127970b848028ce514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2b527de4d3299c3dd18aa0663aba62",
+    "url": "/Chromato/chromato-app/build/index.html"
+  },
+  {
+    "revision": "1e851396e4ec090d3322",
+    "url": "/Chromato/chromato-app/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d6dbb3a09d378192ae9",
+    "url": "/Chromato/chromato-app/build/static/js/2.1fc26d87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chromato/chromato-app/build/static/js/2.1fc26d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e851396e4ec090d3322",
+    "url": "/Chromato/chromato-app/build/static/js/main.ce8b8587.chunk.js"
+  },
+  {
+    "revision": "2cd120b637766c0ead87",
+    "url": "/Chromato/chromato-app/build/static/js/runtime-main.26b759ef.js"
+  }
+]);
